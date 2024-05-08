0d6ad09f240f65980be984c6fcf1d01ed48858da
shabhias <shabhialiyya3101@gmail.com>
2024-05-08 16:27:31 +0700

    [GREEN] implemented user balance model

--- src/main/java/id/ac/ui/cs/advprog/paymentservice/model/Balance.java
+++ src/main/java/id/ac/ui/cs/advprog/paymentservice/model/Balance.java
@@ -1,1 +1,13 @@
+package id.ac.ui.cs.advprog.paymentservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Getter
+@Setter
+public class Balance {
+    private Long custId;
+    private Integer custBalance;
+
+}
