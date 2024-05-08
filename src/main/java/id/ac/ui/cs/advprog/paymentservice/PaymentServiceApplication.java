65414eb031f7a88a8cec91849377a2d6ba688701
shabhias <shabhialiyya3101@gmail.com>
2024-05-08 15:22:55 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
+}
